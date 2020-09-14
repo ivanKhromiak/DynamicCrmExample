d0b2a338411fdf7e4036d06bcb2ee4417a2c08dc
Ivan Khromiak <ivan_khromiak@epam.com>
2020-09-14 20:36:54 +0300

    Add source code

---
+++ TestTask/AccountPlugin.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.Xrm.Sdk;
+
+namespace TestTask
+{
+    public class AccountPlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    entity["new_isValidated"] = false;
+
+                    if (entity["telephone1"] != null)
+                    {
+                        entity["new_isValidated"] = true;
+                    }
+
+                    if (context.MessageName == "Create")
+                    {
+                        service.Create(entity);
+                    }
+
+                    if (context.MessageName == "Update")
+                    {
+                        service.Update(entity);
+                    }
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
